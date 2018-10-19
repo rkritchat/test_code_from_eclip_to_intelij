fd50d36f49bea0b279d9aef51df0968b65d995ca
rkritchat <rkritchat@gmail.com>
2018-10-19 16:40:26 +0700

    test create project fron eclip

---
+++ src/main/java/com/example/demofromeclip/resource/Student.java
@@ -0,0 +1,15 @@
+package com.example.demofromeclip.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class Student {
+	
+	@GetMapping
+	public String test() {
+		return "worked...";
+	}
+}
---
+++ src/test/java/com/example/demofromeclip/DemofromeclipApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demofromeclip;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemofromeclipApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
